d339455f5dcb1d18892144f4712c337473534656
Ben Horn <benhorenn@gmail.com>
2018-04-05 17:13:45 +0300

    versin 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.core.PImage;
 
 
@@ -11,15 +13,36 @@ public class Main extends PApplet{
 
 
 	static String pdfname= "output";
+	static String text= "";
+	static String fontName;
+	static float textSize;
+	static float distance;
 
 	public static void main(String[]args){
-		play("pics");
+		String text ="text on pics";
+		play("pics", text, "Arial", (float)20, 10);
 	}
 
 	static int times;
 	static String folder;
-	public static void play(String path){
+	public static void play(String path, String itext, String ifont, float itextSize, float idistance){
 		folder = path;
+		textSize= itextSize;
+		fontName = ifont;
+		distance = idistance;
+
+		text = fixText(itext);
+		System.out.println(text);
+
+		if(text == null) text = "";
+		if(fontName == null || fontName.isEmpty())
+			fontName = "Arial";
+
+		if(!text.isEmpty() && distance<0)
+			distance = 15;
+		
+		if(!text.isEmpty() && textSize<=0)
+			textSize = 15;
 
 		File f = new File(folder);
 		if(!f.exists())
@@ -27,10 +50,13 @@ public class Main extends PApplet{
 
 		images = listFilesForFolder(f);
 
+
 		times = images.size()/18+1;
 		if(images.size()%18==0)
 			times--;
-		
+
+		System.out.println(images.size()+" t"+times);
+
 		PApplet.main("Main");
 	}
 
@@ -39,9 +65,14 @@ public class Main extends PApplet{
 		ArrayList<String> images = new ArrayList<String>();
 		for (final File fileEntry : folder.listFiles()) {
 
-			System.out.println(fileEntry.getName());
-			if(fileEntry.getName().contains(format))
-				images.add(fileEntry.getName());
+			for(int i=0; i<format.length; i++){
+				//				String ext2 = FilenameUtils.getExtension("bar.exe");
+				if(fileEntry.getName().toUpperCase().contains(format[i])){
+					images.add(fileEntry.getName());
+					break;
+				}
+
+			}
 		}
 		return images;
 	}
@@ -60,33 +91,59 @@ public class Main extends PApplet{
 	}
 
 
-	static String format = "png";
+	static final String[] format ={ "PNG"	, "JPG", "JPEG", "GIF"};
 	static ArrayList<String>  images;
-
-	public void settings() {
-		size((int)mm(420),(int)mm(800)); 
-
-	}
 
 	private String[] getImages() {
 		String[] cuImages = new String[18];
 
 		for(int i =0; i<cuImages.length; i++){
-			if(i<images.size()){
-				cuImages[i] = images.get(i);
-				images.remove(i);
+			if(0<images.size()){
+				cuImages[i] = images.get(0);
+				images.remove(0);
 			}
 		}
 		return cuImages;
+	}
+
+	public static String[] fontList(){
+		return PFont.list();
+	}
+
+	public void setup(){
+		//		size((int)mm(420),(int)mm(800)); 
+
+		String[] fontList = PFont.list();
+//		System.out.println(Arrays.toString(fontList));
+
+		if(textSize>0)
+			myFont = createFont(fontName, textSize);
+
+	}
+	PFont myFont;
+	public void settings() {
+		size((int)mm(420),(int)mm(800)); 
+
+		//		String[] fontList = PFont.list();
+		//		System.out.println(Arrays.toString(fontList));
+		//		
+		//		PFont myFont = createFont(fontName, textSize);
+		//		textAlign(CENTER);
+		//		 textFont(myFont);
 	}
 
 
 	static int count =0;
 	public void draw(){
 
-		if(count == times)
+		if(count ==  times-1){
+			//			sleep(7000);
 			exit();
+		}
+
 		String[] pdfImages = getImages(); 
+		if(pdfImages == null)
+			exit();
 		count ++;
 
 		System.out.println("round");
@@ -101,7 +158,7 @@ public class Main extends PApplet{
 			if(!f.exists())
 				ok = true;
 		}
-		//		pdfname +="-"+n;
+
 		if(n==0)
 			beginRecord(PDF, pdfname+".pdf");
 		else
@@ -115,8 +172,8 @@ public class Main extends PApplet{
 			x=0; y=0;
 			for(int j=0; j<6; j++){
 
-				if(index<pdfImages.length)
-					img = loadImage(pdfImages[index]);
+				if(index<pdfImages.length && pdfImages[index]!=null && !pdfImages[index].isEmpty())
+					img = loadImage(folder+"//"+pdfImages[index]);
 				else break;
 				index++;
 
@@ -126,6 +183,18 @@ public class Main extends PApplet{
 				//rect(mm(x), mm(y), mm(95), mm(95)); //row 2
 
 				image(img,mm(x), mm(y), mm(95), mm(95));
+
+				fill(255);
+				if((i==0 &&(j==0 || j == 5)) || (i==2 && (j==0 || j==5))) 
+					ellipse(mm(x+(float) 47.5),mm(y+(float) 47.5) ,mm(2),mm(2)); 
+
+				//				 textSize(textSize);
+				if(textSize>0){
+					fill(0);
+					textAlign(CENTER, BOTTOM);
+					textFont(myFont);
+					text(text, mm(x+(float) 47.5), mm(y+ (95-distance)));
+				}
 			}
 		}
 
@@ -140,4 +209,75 @@ public class Main extends PApplet{
 		return wantedMM / MM_TO_PIXEL_RATIO;
 	}
 
+	public static String fixText(String text){
+		System.out.println(text);
+		String itext ="";
+		String[] words = text.split(" ");
+
+		String[] strings = new String[words.length];
+		
+		System.out.println(Arrays.toString(words));
+		
+		for(int i=0; i<strings.length; i++)strings[i]="";
+
+		boolean ok = false; int index=0;
+		for(int i=0; i<words.length; i++){
+
+			if(isHebrew(words[i])){
+				strings[index] += words[i]+" ";
+				ok = true;
+			}
+			else{
+				if(ok) index++;
+				strings[index] = words[i]+" ";
+				index++;
+				ok = false;
+			}
+		}
+
+		System.out.println(Arrays.toString(strings));
+		
+		for(int i=0; i<strings.length; i++){
+			if(isHebrew(strings[i]))
+				strings[i] = new String(reverse(strings[i].toCharArray()));
+		}
+
+		for(int i=0; i<strings.length; i++){
+			itext+=strings[i]+" ";
+		}
+
+		return itext;
+	}
+
+	public static boolean isHebrew(String s){
+		if(s==null) return false;
+
+		return     s.contains("à")
+				|| s.contains("á")
+				|| s.contains("â")
+				|| s.contains("ã")
+				|| s.contains("ä")
+				|| s.contains("å")
+				|| s.contains("æ")
+				|| s.contains("ç")
+				|| s.contains("è")
+				|| s.contains("é")
+				|| s.contains("ë")
+				|| s.contains("ì")
+				|| s.contains("î")
+				|| s.contains("ð")
+				|| s.contains("ñ")
+				|| s.contains("ò")
+				|| s.contains("ô")
+				|| s.contains("ö")
+				|| s.contains("÷")
+				|| s.contains("ø")
+				|| s.contains("ù")
+				|| s.contains("ú")
+				|| s.contains("ê")
+				|| s.contains("í")
+				|| s.contains("ï")
+				|| s.contains("ó")
+				|| s.contains("õ");
+	}
 }
---
+++ src/main/java/mainScreen.java
@@ -0,0 +1,233 @@
+import java.awt.Choice;
+import java.awt.EventQueue;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+
+
+public class mainScreen {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextPane textPane_3 ;
+	private Choice choice;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainScreen window = new mainScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mainScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		textField = new JTextField();
+		textField.setBounds(63, 26, 230, 20);
+		textField.setHorizontalAlignment(JTextField.RIGHT);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setFont(new Font("Arial", Font.PLAIN, 14));
+		textPane.setText("\u05D8\u05E7\u05E1\u05D8");
+		textPane.setBounds(303, 26, 72, 20);
+		frame.getContentPane().add(textPane);
+
+		textField_1 = new JTextField();
+		textField_1.setBounds(63, 57, 230, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setEditable(false);
+		textPane_1.setFont(new Font("Arial", Font.PLAIN, 14));
+		textPane_1.setText("\u05D2\u05D5\u05D3\u05DC");
+		textPane_1.setBounds(303, 57, 72, 20);
+		frame.getContentPane().add(textPane_1);
+
+		JTextPane textPane_2 = new JTextPane();
+		textPane_2.setEditable(false);
+		textPane_2.setFont(new Font("Arial", Font.PLAIN, 14));
+		textPane_2.setText("\u05E4\u05D5\u05E0\u05D8");
+		textPane_2.setBounds(304, 88, 72, 20);
+		frame.getContentPane().add(textPane_2);
+
+		final Choice choice = new Choice();
+		choice.setBounds(63, 88, 230, 20);
+		frame.getContentPane().add(choice);
+		choice.setFocusable(false);
+
+		JButton button = new JButton("\u05D4\u05E4\u05E2\u05DC");
+		button.setBounds(180, 199, 89, 23);
+		button.setFocusable(false);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				double fontsize =0;
+				try{
+					if(!textField_1.getText().isEmpty())
+						fontsize = Double.parseDouble(textField_1.getText());
+				}catch(Exception e){return;}
+				double distance = 0;
+				try{
+					if(!textField_1.getText().isEmpty())
+						distance = Double.parseDouble(textField_2.getText());
+				}catch(Exception e){return;}
+				String text = textField.getText();
+				String path = textPane_3.getText();
+				String font = choice.getSelectedItem().toString();
+
+				System.out.println(path);
+				System.out.println(text);
+				System.out.println(font);
+				System.out.println(fontsize);
+				System.out.println(distance);
+
+				Main.play(path, text, font, (float)fontsize, (float) distance);
+
+				//				System.exit(1);
+			}
+		});
+
+		frame.getContentPane().add(button);
+
+		textPane_3 = new JTextPane();
+		textPane_3.setEditable(false);
+		textPane_3.setBounds(63, 145, 312, 20);
+		frame.getContentPane().add(textPane_3);
+		String[] fonts = Main.fontList();
+
+		choice.add("Arial");
+		for(int i=0; i<fonts.length; i++){
+			choice.add(fonts[i]);
+		}
+
+
+
+
+		//		frame = new JFrame();
+		//		frame.setBounds(100, 100, 450, 300);
+		//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//		frame.getContentPane().setLayout(null);
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+		//		FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel File", "xlsx", "xlsm", "xls");
+		//		fileChooser.setFileFilter(filter);
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fileChooser.setAcceptAllFileFilterUsed(false);
+
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			SwingUtilities.updateComponentTreeUI(fileChooser);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String path = "";
+		int result = fileChooser.showOpenDialog(frame);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fileChooser.getSelectedFile();
+			path = selectedFile.getAbsolutePath();
+
+			System.out.println("Selected file: " + path);
+		}
+
+		textPane_3.setText(path);
+		
+
+		textField_2 = new JTextField();
+		textField_2.setBounds(63, 114, 230, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+
+		JTextPane textPane_4 = new JTextPane();
+		textPane_4.setFont(new Font("Arial", Font.PLAIN, 14));
+		textPane_4.setEditable(false);
+		textPane_4.setText("\u05DE\u05E8\u05D7\u05E7 \u05DE\u05D4\u05EA\u05D7\u05EA\u05D9\u05EA");
+		textPane_4.setBounds(304, 114, 120, 20);
+		frame.getContentPane().add(textPane_4);
+
+		
+//		Popup popup = new Popup();
+//		popup.add(textField);
+//		
+//		Popup popup1 = new Popup();
+//		popup1.add(textField_1);
+//		
+//		Popup popup2 = new Popup();
+//		popup2.add(textPane_2);
+//		
+//		Popup popup3 = new Popup();
+//		popup3.add(textPane_3);
+
+		//		try {
+		//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		//			SwingUtilities.updateComponentTreeUI(frame);
+		//
+		//		} catch (ClassNotFoundException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		} catch (InstantiationException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		} catch (IllegalAccessException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		} catch (UnsupportedLookAndFeelException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+
+	}
+}
